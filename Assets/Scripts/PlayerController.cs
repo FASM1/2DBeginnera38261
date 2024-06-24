99bd10f712d3d9c1379aca8d07d69768b5e655cd
FASM1 <filipeari@live.com.pt>
2024-06-24 23:30:44 +0100

    2.Define an Input Action
    2.Define an Input Action@38261

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public InputAction LeftAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
