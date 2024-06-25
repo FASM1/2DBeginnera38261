5b5a3138fecd8f5a702f5bd0c7dd49f0fcc50033
FASM1 <filipeari@live.com.pt>
2024-06-25 03:01:10 +0100

    5.Increase the player character health
    5.Increase the player character health@38261

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public class HeathCollectible : MonoBehaviour
     {
-        
+
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Object that entered the trigger: " + other);
+        PlayerController controller = other.GetComponent<PlayerController>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(1);
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
